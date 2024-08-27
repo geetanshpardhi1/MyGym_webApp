df124fc0ea045da7cee00fcfd27883c02cec1ec5
geetanshpardhi1 <geetanshpardhi@gmail.com>
2024-08-27 15:31:56 +0530

    changes to frontend for implemeting redux and all

---
+++ frontend/mygym_app/src/store/authSlice.js
@@ -0,0 +1,22 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const authSlice = createSlice({
+//   name: 'auth',
+//   initialState: {
+//     accessToken: null,
+//     user: null,
+//   },
+//   reducers: {
+//     setCredentials: (state, action) => {
+//       state.accessToken = action.payload.accessToken;
+//       state.user = action.payload.user;
+//     },
+//     logout: (state) => {
+//       state.accessToken = null;
+//       state.user = null;
+//     },
+//   },
+// });
+
+// export const { setCredentials, logout } = authSlice.actions;
+// export default authSlice.reducer;
